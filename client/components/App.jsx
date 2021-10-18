ed0caf5067bbc12eb99a61dfcc1280b453094219
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-10-18 16:07:58 +1300

    prepare boilerplate for project beginning

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import Fruit from './Fruit'
-
 const App = () => {
   return (
     <>
-      <h1>Hola! Fruit time!</h1>
-      <Fruit />
+      <h1>Contact Book</h1>
     </>
   )
 }
--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,5 @@ const config = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
-
-const getAllFruit = (db = connection) => {
-  return db('fruit')
-}
-
 module.exports = {
-  getAllFruit
 }
