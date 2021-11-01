c1eeb81900610b9f8902207b27bcd734cced7e0b
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-11-01 15:40:26 +1300

    working on contacts display

---
+++ client/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Contact () {
+    return  (
+        <>
+            <p>Hello</p>
+        </>
+    )
+}
+
+export default Contact
--- client/components/Contacts.jsx
+++ client/components/Contacts.jsx
@@ -1,10 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { connect } from 'react-redux'
 
-function Contacts () {
+import Contact from "./Contact"
+
+import { getContacts } from "../actions/contacts"
+
+function Contacts ({dispatch, contacts}) {
+    useEffect(() => {
+        dispatch(getContacts())
+    }, [])
+
     return  (
         <>
+            {contacts?
+            <>
+                {contacts.map(contact => {
+                    return (
+                        <Contact key={contact.id} contact={contact} />
+                    )
+                })}
+            </>
+            : <p>Loading...</p>}
         </>
     )
 }
 
-export default Contacts
+function mapStateToProps (globalState) {
+    return {
+      contacts: globalState.contacts
+    }
+  }
+  
+export default connect(mapStateToProps)(Contacts)
