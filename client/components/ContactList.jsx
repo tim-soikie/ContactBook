5f302be3bfbb32adc920a03f1232239e02105e34
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-11-05 12:36:26 +1300

    preparing details view

--- client/components/ContactList.jsx
+++ client/components/ContactList.jsx
@@ -5,10 +5,6 @@ function ContactList ({contact}) {
     return  (
         <div className="contactRow">
             <p className="contactColumn">{contact.name}</p>
-            <p className="contactColumn">{contact.email}</p>
-            <p className="contactColumn">{contact.phone}</p>
-            <p className="contactColumn">{contact.birthdate}</p>
-            <p className="contactColumn">{contact.last_chat}</p>
         </div>
     )
 }
--- client/components/Contacts.jsx
+++ client/components/Contacts.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { connect } from 'react-redux'
 
 import ContactList from "./ContactList"
 
 import { getContacts } from "../actions/contacts"
+import ContactDetails from "./ContactDetails"
 
 function Contacts ({dispatch, contacts}) {
     
@@ -11,24 +12,37 @@ function Contacts ({dispatch, contacts}) {
         dispatch(getContacts())
     }, [])
 
+    const [currentContact, setCurrentContact] = useState({});
+
+    const findCurrentContactDetails = (id) => {
+        return setCurrentContact(contacts.find(contact => contact.id === id ))
+    }
+
     return  (
         <>
             <div className="contactRowHeader">
-                <p className="contactColumn">Name</p>
-                <p className="contactColumn">Email</p>
-                <p className="contactColumn">Phone</p>
-                <p className="contactColumn">Birthday</p>
-                <p className="contactColumn">Last Chat</p>
+                <p className="contactColumn">Contacts</p>
             </div>
-            {contacts?
-            <>
-                {contacts.map(contact => {
-                    return (
-                        <ContactList key={contact.id} contact={contact} />
-                    )
-                })}
-            </>
-            : <p>Loading...</p>}
+        <div className="contactContainer">
+            <div className="contactColumnList">
+                {contacts?
+                <>
+                    {contacts.map(contact => {
+                        return (
+                            <ContactList key={contact.id} contact={contact} />
+                        )
+                    })}
+                </>
+                : <p>Loading...</p>}
+            </div>
+            <div className="contactDetails">
+                {currentContact.id?
+                <>
+                    <ContactDetails contact={currentContact}/>
+                </>
+                : <p>Select A contact to view details</p>}
+            </div>
+        </div>
         </>
     )
 }
